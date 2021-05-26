8f6223885ce61e5796550b358cae4cb157867084
Andrey0295 <andreevicandrej00@gmail.com>
2021-05-26 09:35:22 +0300

    setting enviroment

---
+++ README.old.md
@@ -0,0 +1,1 @@
+patternica-test-task
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from './components/Container/Container';
+
+const App = () => {
+  return (
+    <Container>
+      <div>
+        <h1>Hello React</h1>
+      </div>
+    </Container>
+  );
+};
+
+export default App;
